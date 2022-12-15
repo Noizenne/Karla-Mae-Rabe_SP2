cb367980241748f12bc74915beca1e9522a3ace6
Noizenne <karlamae2405@gmail.com>
2022-12-15 19:35:44 +0100

    Fixed search

--- src/js/handlers/search.mjs
+++ src/js/handlers/search.mjs
@@ -1,40 +1,45 @@
 import { getListings } from "../api/listings/read.mjs";
-
+import { newDate } from "../tool/countdown.mjs";
 
 export async function searchListener() {
+  const searchInput = document.querySelector("#searchInput");
+  const searchForm = document.querySelector("#search");
+  const listingsContainer = document.querySelector(".searchResult");
 
-    try {
-        const searchInput = document.querySelector("#searchInput");
-        const searchForm = document.querySelector("#search");
-        const listingsContainer = document.querySelector(".searchResult");
+  searchForm.addEventListener("keyup", async (event) => {
+    event.preventDefault();
 
-        searchForm.addEventListener("keyup", async (event) => {
-            event.preventDefault();
+    const listings = await getListings();
+    console.log(listings);
 
-            const listings = await getListings();
-            console.log(listings)
-            
-            const filteredListings = listings.filter((listing) => {
-                const title = listing.title.toLowerCase();
+    const filteredListings = listings.filter((listing) => {
 
-                const searchValue = searchInput.value.toLowerCase();
-                console.log(searchValue)
+      const endsAt = listing.endsAt;
+  const endsAtDate = newDate(endsAt);
+      const title = listing.title.toLowerCase();
 
-                if(title.includes(searchValue)) {
-                    listingsContainer.innerHTML = `<a href="/Treasures/listings/listing/index.html?id=${listing.id}">
-                    <div class="d-flex justify-content-center">${listing.title}</div>
-                     <div class="d-flex justify-content-center small">${listing.endsAt}</div></a>`;
-                    return true;
-                }
-            });
+      const searchValue = searchInput.value.toLowerCase();
+      console.log(searchValue);
 
-            if(filteredListings === 0) {
-                listingsContainer.innerHTML = `<p>No matches</p>`
-            }
+      if (title.includes(searchValue)) {
+        listingsContainer.innerHTML = `<div class="listing">
+                    <a href="/Treasures/listings/listing/index.html?id=${listing.id}">
+                    <div class="d-flex justify-content-center text-align-center">${listing.title}</div>
+                     <div class="d-flex justify-content-center date"><i class="fa-solid fa-hourglass-start"></i>${endsAtDate}</div>
+                     <div class="d-flex justify-content-center"><img style="solid 1px white" src="${listing.media}"</img></div></a>
+                     <div>`;
+        return true;
+      }
+    });
 
-            getListings(filteredListings, listingsContainer)
-        } )
-    } catch(error) {
-console.log("Error")
+    if (filteredListings === 0) {
+      listingsContainer.innerHTML = `<p>No matches</p>`;
     }
+
+    getListings(filteredListings, listingsContainer);
+  });
 }
+
+/*<a href="/Treasures/listings/listing/index.html?id=${listingsData.id}">
+   <div class="d-flex justify-content-center text-align-center">${listingsData.title}</div>
+    <div class="date d-flex justify-content-center align-items-center"><i class="fa-solid fa-hourglass-start"></i>${endsAtDate}</div></a> */
